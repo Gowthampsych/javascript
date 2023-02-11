df0f9c89343ab6baeca1e9fbdbc47f14260f325b
psych <gowtham.2011015@srec.ac.in>
2023-02-11 18:54:11 +0530

    Changes by prasath in js file comments

---
+++ index.js
@@ -0,0 +1,58 @@
+let count = 0
+//let his=[]
+let countEl = document.getElementById("count")
+//let num=document.getElementById('numer')
+
+function increment() {
+    count +=  1
+    countEl.innerText = count
+}
+
+function save() {
+    //his.push(count)
+    //num.innerText=his.toString();
+    
+    
+
+}
+
+
+
+/*
+// next lesson - write your first string
+
+
+let message = "You have tree new notification"
+console.log(message)
+
+ let username ="per"
+ console.log(username)
+
+ messageToUser = message + " "+ username
+
+ console.log(messageToUser)
+
+
+
+ let name = "Gowtham"
+ let greeting = "Hi, my name is "
+
+ let myGreeting = greeting + name 
+
+ console.log(myGreeting)
+
+
+
+  let welcomeEl = document.getElementById("welcome-el")
+
+  let name = "Gowtham"
+  let greeting = "Hi!, how are you "
+
+  welcomeEl.innerText = greeting + name
+
+  // welcomeEl.innerText = welcomeEl.innerText +  " 💔"
+
+  welcomeEl.innerText +=  " 💔"
+
+*/
+
