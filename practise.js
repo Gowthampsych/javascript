88b02b36ea39b4e0f26bd4c396e388130569e0ce
psych <gowtham.2011015@srec.ac.in>
2023-02-21 18:41:31 +0530

    new changes

--- practise.js
+++ practise.js
@@ -1,9 +1,79 @@
 
 // condition:
 
+// if
+/*
 if (confirm("Are you John Smith?"))
 {
     console.log("Hello John, how are you?");
 } else {
     console.log("Then what is your name?");
 }
+
+// if condition with === 
+
+number = 123;
+function my(number)
+{
+    if (number==="123")  // ===
+    {
+        console.log("number is 123")
+    }
+    else
+    {
+        console.log("number is not 123")
+    }
+}
+
+my(23);
+my(234);
+my(123);
+
+
+// if condition with ==
+number = 123;
+function my(number)
+{
+    if (number==123)  // ==
+    {
+        console.log("number is 123")
+    }
+    else
+    {
+        console.log("number is not 123")
+    }
+}
+
+my(23);
+my(234);
+my(123);
+
+
+// inequality operator:
+var i = 23
+var j = 234
+ if (i<j)
+ {
+    console.log("i is lesser than j")
+ }
+ else
+ {
+    console.log("i is greater than j")
+ }
+
+ // AND and OR operator
+var i = 23;
+var j = 234;
+var f = 123;
+ if (i<j && j>f)
+ {
+    console.log("i is lesser than j AND j is greater than f");
+ }
+ else if(i<j || j<f)
+ {
+    console.log("i is lesser than j OR j is lesser than f");
+ }
+
+ */
+
+
